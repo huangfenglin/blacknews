e0bc8356b66b9adb73782d35bf9bfe6b9b577f61
huangfenglin <effort0755@163.com>
2019-10-27 13:21:56 +0800

    头部导航组件

---
+++ src/components/headerMiddle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <span class="iconfont iconjiantou2" @click="back"></span>
+    <div class="title"><span>{{title}}</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["title"],
+  methods: {
+    back() {
+      this.$emit('previousPage')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .header {
+    display: flex;
+    height: 13.889vw;
+    align-items: center;
+    justify-content: space-between;
+    span,.title {
+      flex: 50%
+    }
+    .iconfont {
+        padding: 0 2.778vw;
+    }
+    .title{
+      text-align: center;
+      font-weight: bold;
+      transform: translateX(-50%)
+    }
+  }
+</style>
